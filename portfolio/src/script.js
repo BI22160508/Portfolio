de6279dbb9ab61a3faace7d621a0314c945b7c9b
BI22160508 <ng_chuang_bi22@iluv.ums.edu.my>
2026-02-07 14:57:41 +0800

    Delete portfolio/src directory

--- portfolio/src/script.js
+++
@@ -1,5 +0,0 @@
-// Set footer year automatically
-document.addEventListener("DOMContentLoaded", () => {
-  const y = document.getElementById("y");
-  if (y) y.textContent = new Date().getFullYear();
-});
